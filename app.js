88861440aed53b2bb3b9beecaaa05c84061def3e
vijaydevkate <vijaydevkate98@gmail.com>
2023-01-26 18:06:12 -0500

    done

--- app.js
+++ app.js
@@ -2,11 +2,31 @@ var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
 var outputDiv = document.querySelector("#output");
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input
+}
+
+//when server is down or erroe occur
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something wrong with server, try again after some time")
+}
 
 btnTranslate.addEventListener("click", function clickHandler(){
+   var inputText = textInput.value; // takinig input
 
-    outputDiv.innerText = "jsjsjsjjsjsjsj" + textInput.value;
-   
+    //Calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText; // output
+         })
+    .catch(errorHandler)
+    
 })
 
